c8f4bb48fc0a6f3a6912d49b416f4645b6d8d82e
Bryan Gruneberg <bryangruneberg@gmail.com>
2015-01-08 22:41:14 +0200

    first commit

--- yii2init/recipes/initdevelopment.rb
+++ yii2init/recipes/initdevelopment.rb
@@ -3,6 +3,10 @@
 # Recipe:: initdevelopment
 #
 
-execute "init" do
-  command "./current/init --env=Development --overwrite=y"
+node[:deploy].each do |application, deploy|
+  execute "init" do
+    init_script = "#{deploy[:deploy_to]}/current/init"
+    command "#{init_script} --env=Development --overwrite=y"
+    cwd "#{deploy[:deploy_to]}/current"
+  end
 end
--- yii2init/recipes/initproduction.rb
+++ yii2init/recipes/initproduction.rb
@@ -3,6 +3,10 @@
 # Recipe:: initproduction
 #
 
-execute "init" do
-  command "./current/init --env=Production --overwrite=y"
+node[:deploy].each do |application, deploy|
+  execute "init" do
+    init_script = "#{deploy[:deploy_to]}/current/init"
+    command "#{init_script} --env=Production --overwrite=y"
+    cwd "#{deploy[:deploy_to]}/current"
+  end
 end
