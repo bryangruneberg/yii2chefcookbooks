4ba17e9a205471fd2749400cd04782042491cc74
Bryan Gruneberg <bryangruneberg@gmail.com>
2015-01-08 23:15:29 +0200

    Added yii2composer

--- yii2composer/recipes/install.rb
+++ yii2composer/recipes/install.rb
@@ -2,14 +2,13 @@
 # Taken from:
 # http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
 #
-node[:deploy].each do |application, deploy|
-  script "install_composer" do
-    interpreter "bash"
-    user "root"
-    cwd "/usr/local/bin"
-    code <<-EOH
-    curl -s https://getcomposer.org/installer | php
-    php composer.phar install --no-dev --no-interaction --optimize-autoloader
-    EOH
-  end
-end 
+script "install_composer" do
+  interpreter "bash"
+  user "root"
+  cwd "/usr/local/bin"
+  code <<-EOH
+  curl -s https://getcomposer.org/installer | php
+  php composer.phar install --no-dev --no-interaction --optimize-autoloader
+  cp composer.phar composer
+  EOH
+end
--- yii2composer/recipes/update.rb
+++ yii2composer/recipes/update.rb
@@ -2,14 +2,12 @@
 # Taken from:
 # http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
 #
-node[:deploy].each do |application, deploy|
-  script "install_composer" do
-    interpreter "bash"
-    user "root"
-    cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
-    curl -s https://getcomposer.org/installer | php
-    php composer.phar update
-    EOH
-  end
-end 
+script "install_composer" do
+  interpreter "bash"
+  user "root"
+  cwd "/usr/local/bin"
+  code <<-EOH
+  curl -s https://getcomposer.org/installer | php
+  php composer.phar update
+  EOH
+end
