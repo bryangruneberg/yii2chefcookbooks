0ffb8ef75690f11e06d2047981f4fbae8ea09569
Bryan Gruneberg <bryan@daddysdeals.co.za>
2015-01-08 22:00:35 +0200

    first commit

---
+++ metadata.rb
@@ -0,0 +1,5 @@
+maintainer       "Bryan Gruneberg"
+maintainer_email "bryangruneberg@gmail.com"
+description      "Runs Yii2 init scripts"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1"
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: yii2init
+# Recipe:: default
+#
+
+execute "init" do
+  command "./init"
+end
