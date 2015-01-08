dd46c340b0cdc81de4a30b3f7b1e1c32a5e35434
Bryan Gruneberg <bryangruneberg@gmail.com>
2015-01-08 23:18:16 +0200

    Added yii2composer

--- yii2init/recipes/initproduction.rb
+++ yii2init/recipes/initproduction.rb
@@ -13,7 +13,7 @@ node[:deploy].each do |application, deploy|
     interpreter "bash"
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
-    php composer.phar global require "fxp/composer-asset-plugin:1.0.0-beta4"
+    composer global require "fxp/composer-asset-plugin:1.0.0-beta4"
     EOH
   end
 end
